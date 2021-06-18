a0c5d5b9c46a1ab7b9e2d20fe61357b63b8f627f
Tony Hirst <tony.hirst@gmail.com>
2021-06-18 17:48:47 +0100

    Update version, long desc

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 import setuptools
+from os import path
+
+def get_long_description():
+    with open(
+        path.join(path.dirname(path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
 
 setuptools.setup(
     name="nb_extension_tagstyler",
     packages=['tagstyler'],
-    version='0.0.1',
+    version='0.0.2',
     include_package_data=True,
     install_requires=[
         'notebook'
     ],
+    description="Style classic notebook cells according to simple bootstrap style cell tags.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     data_files=[
         # like `jupyter nbextension install --sys-prefix`
         ("share/jupyter/nbextensions/tagstyler", [
